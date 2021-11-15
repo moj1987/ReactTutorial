c0e196bfe61a7d8c80f7a9b5a0db0274b03ca9c8
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-11-15 10:29:00 -0500

    Implement cognito authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify from "@aws-amplify/core";
+import awsExports from "./aws-exports";
+import { Auth } from "aws-amplify";
+
+Amplify.configure(awsExports);
+
+async function signOut() {
+  try {
+    await Auth.signOut();
+  } catch (error) {
+    console.log("Error signing out: ", error);
+  }
+}
+
+async function globalSignOut() {
+  try {
+    await Auth.signOut({ global: true });
+  } catch (error) {
+    console.log("Error signing out: ", error);
+  }
+}
 
 const App = () => {
   return (
     <div className="App">
-      <Signup />
+      {/* <Signup />
       <br />
-      <Login />
+      <Login /> */}
+      <br />
+      <button onClick={signOut}>Sign out</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={globalSignOut}>Sign out from all devices</button>
     </div>
   );
 };
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
